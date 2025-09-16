2df81ed45434be97b8fb79068025ea837438bc49
Ds1ma <fisodhid745@gmail.com>
2025-09-16 18:47:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите n");
+double n = Convert.ToDouble(Console.ReadLine());
+for (double i = 0; i < n; i++)
+{
+    if (i % 3 == 0 && i % 5 !=0)
+    {
+        Console.WriteLine(i);
+    }
+}
